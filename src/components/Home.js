a9830e9119a36b58ace132ac1ac590b61500f6f8
John Jiang <john.jiang@checkit.net>
2020-05-07 22:47:12 +0100

    Added buttons to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,41 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import {Button, Grid} from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import {createStyles, makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme =>
+  createStyles({
+    questionBox: {
+      padding: theme.spacing(8)
+    },
+    question: {
+      border: 1
+    },
+    answer: {
+      padding: theme.spacing(8)
+    }
+  })
+);
 
 function Home() {
+  const classes = useStyles();
+
   return (
-    <h2 className="Home">Hello world</h2>
+    <Grid
+    container={true}
+    direction="column"
+    justify="center"
+    spacing={3}
+    className={classes.questionBox}
+    alignItems="center"
+  >
+    <Grid item={true} xs={12}>
+      <Button variant="contained" color="primary" component={Link} to='/questions'>Activity 1</Button>
+    </Grid>
+    <Grid item={true} xs={12}>
+      <Button variant="contained" color="primary" component={Link} to='/questions'>Activity 2</Button>
+    </Grid>
+  </Grid>
   );
 }
 
--- src/components/Questions.js
+++ src/components/Questions.js
@@ -3,14 +3,20 @@ import PropTypes from 'prop-types';
 import HomeIcon from '@material-ui/icons/Home';
 import { Link } from 'react-router-dom';
 import Question from './Question';
-import { Button } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 
 function Questions(props) {
   return (
-    <>
-      <Question content="What is your favourite food?" />
-      <Button component={Link} to="/"><HomeIcon/></Button>
-    </>
+    <Grid container={true}>
+      <Grid item={true} xs={12}>
+        <Question content="What is your favourite food?" />
+      </Grid>
+      <Grid item={true} xs={12} alignItems='center'>
+        <Button component={Link} to="/"><HomeIcon/></Button>
+      </Grid>
+
+    </Grid>
   );
 }
 
