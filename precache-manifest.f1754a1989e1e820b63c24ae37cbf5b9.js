eb2ea1558eaa4217ad66fca1ce1dd27f54f3769e
John Jiang <john.jiang@checkit.net>
2020-05-07 22:08:14 +0100

    Updates

---
+++ precache-manifest.f1754a1989e1e820b63c24ae37cbf5b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f48716ae64f7623751df59e0b12498e3",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "44b98474099bf057aa15",
+    "url": "/quiz-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e18e616ea8e7a506e9b6",
+    "url": "/quiz-app/static/js/2.663b5ab7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/quiz-app/static/js/2.663b5ab7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44b98474099bf057aa15",
+    "url": "/quiz-app/static/js/main.1f686c6d.chunk.js"
+  },
+  {
+    "revision": "cf0f19b536c77e8ae81a",
+    "url": "/quiz-app/static/js/runtime-main.af2ad936.js"
+  }
+]);
