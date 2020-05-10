25ef03fa299c82105f7245830432bccb9709d354
John Jiang <john.jiang@checkit.net>
2020-05-10 11:54:00 +0100

    Upgrade npm modules and App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+const mockStore = configureMockStore([thunk]);
+
+it('renders without crashing', () => {
+  // Arrange
+  const initialState = {
+    activities: {
+      byId: {}
+    }
+  };
+
+  const div = document.createElement('div');
+
+  // Act
+  const { getByText } = render((
+      <Provider store={mockStore(initialState)}>
+          <App/>
+      </Provider>
+  ), div);
+
+  // Assert
+  const linkElement = getByText(/Quiz/i);
   expect(linkElement).toBeInTheDocument();
 });
